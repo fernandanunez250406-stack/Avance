4a05bbc9cfe469e2921315503434ebfec22b2ba1
fernandanunez250406-stack <fernandanunez250406@gmail.com>
2026-01-27 07:41:09 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,20 @@
+class Login {
+    constructor (usuario, contraseña) {
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+    }
+
+}
+
+class Registro {
+    constructor (nombre,f_nacimiento,email,direccion,usuario,contraseña) {
+        this.nombre = nombre;
+        this.f_nacimiento = f_nacimiento;
+        this.email = email;
+        this.direccion = direccion;
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+        }    
+
+  
+    }
